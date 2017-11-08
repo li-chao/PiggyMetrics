2c0b8e4dd11ba56433ac50f5717b858d54ee9337
Li Chao <lichao@ccmid.com>
2017-11-08 23:53:51 +0800

    完善测试

--- config/src/test/java/com/piggymetrics/config/ConfigApplicationTests.java
+++ config/src/test/java/com/piggymetrics/config/ConfigApplicationTests.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -21,26 +22,30 @@ import org.springframework.util.MultiValueMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"CONFIG_SERVICE_PASSWORD = pwd"})
 public class ConfigApplicationTests {
+    
     
     @Autowired
     private TestRestTemplate restTemplate;
+    
 
 	@Test
 	public void configurationAvailable() {
 		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = restTemplate.getForEntity(
+		ResponseEntity<Map> response = restTemplate.withBasicAuth("user", "pwd").getForEntity(
 				"/app/cloud", Map.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
 
 	@Test
 	public void envPostAvailable() {
 		MultiValueMap<String, String> form = new LinkedMultiValueMap<String, String>();
+		
 		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = restTemplate.postForEntity(
-				"/admin/env", form, Map.class);
-		assertEquals(HttpStatus.OK, entity.getStatusCode());
+		ResponseEntity<Map> response = restTemplate.withBasicAuth("user", "pwd").postForEntity(
+				"/env", form, Map.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
 
 }
